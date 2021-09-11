29ac7a5a12235ebac32f4ed6868f2e67399e4abd
lentrix <hawkmanlentrix@gmail.com>
2021-09-11 10:40:10 +0800

    verification lname fname

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,10 +33,10 @@ class AuthController extends Controller
             return back()->with('Error',"Account with IDNumber: $user->idnum was already verified on $dateStr. If you are the owner of this account and you did not verify this yourself, please contact any of the SSG officers for account recovery.");
         }
 
-        if($user->lname!==$request->lname) {
+        if($user->lname!=$request->lname) {
             return back()->withInput()->with('Error', 'The last name does not match');
         }
-        if($user->fname!==$request->lname) {
+        if($user->fname!=$request->lname) {
             return back()->withInput()->with('Error', 'The first name does not match');
         }
 
