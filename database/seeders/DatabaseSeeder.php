3422dae26c578c2d47ce51aab2f17a29238525df
lentrix <hawkmanlentrix@gmail.com>
2021-09-12 13:46:55 +0800

    lock

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(UserSeeder::class);
         $this->call(CandidateSeeder::class);
+        $this->call(VoteSeeder::class);
     }
+
 }
